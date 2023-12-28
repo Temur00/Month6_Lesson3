5eea79ed10238212b807a0df864501f688c0706c
Temur <shernazarovtemurcom001@gmail.com>
2023-12-28 17:33:12 +0500

    added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,27 @@
 import Logo from "./logo.png";
 import mainPic from "./mainPic.jpg";
+import cer1 from "./sertificate1.png";
+import cer2 from "./sertificate2.png";
+import cer3 from "./sertificate3.png";
+import cer4 from "./sertificate4.png";
+import cer5 from "./sertificate5.png";
+import cream1 from "./cream1.png";
+import cream2 from "./cream2.png";
+import cream3 from "./cream3.png";
+import cream4 from "./cream4.png";
+import cream5 from "./cream5.png";
 
-export { Logo, mainPic };
+export {
+  Logo,
+  mainPic,
+  cer1,
+  cer2,
+  cer3,
+  cer4,
+  cer5,
+  cream1,
+  cream2,
+  cream3,
+  cream4,
+  cream5,
+};
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Hero.scss";
 import { mainPic } from "../../assets/images";
+import { cer1 } from "../../assets/images";
+import { cer2 } from "../../assets/images";
+import { cer3 } from "../../assets/images";
+import { cer4 } from "../../assets/images";
+import { cer5 } from "../../assets/images";
+import { cream1 } from "../../assets/images";
+import { cream2 } from "../../assets/images";
+import { cream3 } from "../../assets/images";
+import { cream4 } from "../../assets/images";
+import { cream5 } from "../../assets/images";
 
 function Hero() {
   return (
@@ -50,6 +60,40 @@ function Hero() {
                 туб (ETMA), что подтверждает сильную позицию бренда и на рынке
                 Европы.{" "}
               </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="third-items">
+            <div className="texts">
+              <p>Качество продукции подтверждают </p>
+              <p> сертификаты</p>
+              <div className="certificates">
+                <img src={cer1} alt="img" />
+                <img src={cer2} alt="img" />
+                <img src={cer3} alt="img" />
+                <img src={cer4} alt="img" />
+                <img src={cer5} alt="img" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="fourth-items">
+            <div className="texts">
+              <p>Наша </p>
+              <p>продукция</p>
+              <div className="creams">
+                <img src={cream1} alt="img" />
+                <img src={cream2} alt="img" />
+                <img src={cream3} alt="img" />
+                <img src={cream4} alt="img" />
+                <img src={cream5} alt="img" />
+              </div>
             </div>
           </div>
         </div>
